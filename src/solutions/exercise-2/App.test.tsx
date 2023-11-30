03827f3e4e2ea4643d6298e288864c215d1fa656
Anaïs Boo <anaisb@hokla.com>
2023-11-30 12:33:27 +0100

    feat: add exercise 2 solution

--- src/solutions/exercise-2/App.test.tsx
+++ src/solutions/exercise-2/App.test.tsx
@@ -1,13 +1,21 @@
 
 import { render, screen } from "@testing-library/react";
-import App from "../../App";
+import App, { storyIds } from "../../App";
+import axios from "axios";
 
-
+jest.mock('axios');
 describe("App component with mocked Axios", () => {
   describe("Story view", () => {
     it("should display the story name", async () => {
-      // Build
+      const mockStoryDetails = [
+        { title: "Story 1", url: "http://example.com/story1", score: 100 },
+        { title: "Story 2", url: "http://example.com/story2", score: 200 },
+      ];
 
+      // Mock the get function of axios to return the mockStoryDetails responses
+      storyIds.forEach((id, index) => {
+        (axios.get as jest.Mock).mockResolvedValueOnce({ data: mockStoryDetails[index] });
+      });
       render(<App />);
 
       // Check
--- src/solutions/exercise-2/__mocks__/mockAxios.ts
+++
@@ -1,4 +0,0 @@
-export const get = jest.fn(()=> [
-    { title: "Story 1", url: "http://example.com/story1", score: 100 },
-    { title: "Story 2", url: "http://example.com/story2", score: 200 },
-  ])
